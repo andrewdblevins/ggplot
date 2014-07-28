1760ad519a5022806fb396d719d20cbbf9c0a0c4
Hassan Kibirige <has2k1@gmail.com>
2014-07-28 04:47:55 -0500

    Fix deepcopy and facets
    - For geom, stat and layer do not copy the data
    - Override the previous faceting implementation

--- ggplot/geoms/__init__.py
+++ ggplot/geoms/__init__.py
@@ -21,16 +21,13 @@ from .geom_tile import geom_tile
 from .geom_vline import geom_vline
 
 # misc
-from .facet_grid import facet_grid
-from .facet_wrap import facet_wrap
 from .chart_components import *
 
-__facet__ = ['facet_grid', 'facet_wrap']
 __geoms__ = ['geom_abline', 'geom_area', 'geom_bar', 'geom_boxplot', 'geom_density',
              'geom_histogram', 'geom_hline', 'geom_jitter', 'geom_line',
              'geom_now_its_art', 'geom_path', 'geom_point', 'geom_rect',
              'geom_step', 'geom_smooth', 'geom_text', 'geom_tile',
              'geom_vline']
 __components__ = ['ylab', 'xlab', 'ylim', 'xlim', 'labs', 'ggtitle']
-__all__ = __geoms__ + __facet__ + __components__
+__all__ = __geoms__ + __components__
 __all__ = [str(u) for u in __all__]
